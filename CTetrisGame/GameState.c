37ba07fba85e08366ff78844cbafc1d42558b70d
Bohdan <oleoleqaz@gmail.com>
2025-08-07 12:18:31 +0300

    Reworked memory allocation and cleanup system for dynamic arrays

--- CTetrisGame/GameState.c
+++ CTetrisGame/GameState.c
@@ -2,6 +2,15 @@
 
 void InitGameState(GameState* state)
 {
+	initMapArr(state);
+	initFilledObjectArr(state);
+	state->objCurrentPos.x = START_POS_X;
+	state->objCurrentPos.y = START_POS_Y;
+
+}
+
+void initMapArr(GameState* state)
+{
 	state->mapArr = malloc(MAP_HEIGHT * sizeof(char*));
 	if (state->mapArr == NULL) {
 		return;
@@ -9,11 +18,7 @@ void InitGameState(GameState* state)
 	for (int i = 0; i < MAP_HEIGHT; i++) {
 		state->mapArr[i] = malloc(MAP_WIDTH * sizeof(char));
 		if (state->mapArr[i] == NULL) {
-			for (int k = 0; k < i; k++) {
-				free(state->mapArr[k]);
-			}
-			free(state->mapArr);
-			state->mapArr = NULL;
+			clearFailedState(&state->mapArr, i);
 			return;
 		}
 		for (int j = 0; j < MAP_WIDTH; j++) {
@@ -25,21 +30,52 @@ void InitGameState(GameState* state)
 			}
 		}
 	}
+}
 
-	state->objCurrentPos.x = START_POS_X;
-	state->objCurrentPos.y = START_POS_Y;
+void initFilledObjectArr(GameState* state)
+{
+	const int filledObjectHeight = MAP_HEIGHT - 2;
+	const int filledObjectWidth = MAP_WIDTH - 2;
 
+	state->FilledObjectArr = malloc(filledObjectHeight * sizeof(char*));
+	if (state->FilledObjectArr == NULL) {
+		return;
+	}
+	for (int i = 0; i < filledObjectHeight; i++) {
+		state->FilledObjectArr[i] = malloc(filledObjectWidth * sizeof(char));
+		if (state->FilledObjectArr[i] == NULL) {
+			clearFailedState(&state->FilledObjectArr, i);
+			return;
+		}
+		for (int j = 0; j < filledObjectWidth; j++) {
+			state->FilledObjectArr[i][j] = ' ';
+		}
+	}
+}
+
+void clearFailedState(char *** array, int index)
+{
+	for (int k = 0; k < index; k++) {
+		free(*array[k]);
+	}
+	free(*array);
+	*array = NULL;
+}
+
+void clearGameDoubleArray(char*** array, int height)
+{
+	for (int i = 0; i < height; i++) {
+		free(*array[i]);
+	}
+
+	free(*array);
+	*array = NULL;
 }
 
 void clearGameState(GameState* state)
 {
-
-	for (int i = 0; i < MAP_HEIGHT; i++) {
-		free(state->mapArr[i]);
-	}
-
-	free(state->mapArr);
-	state->mapArr = NULL;
+	clearGameDoubleArray(&state->mapArr, MAP_HEIGHT);
+	clearGameDoubleArray(&state->FilledObjectArr, MAP_HEIGHT-2);
 }
 
 
--- CTetrisGame/GameState.h
+++ CTetrisGame/GameState.h
@@ -7,12 +7,17 @@
 
 typedef struct {
     char** mapArr;
+    char** FilledObjectArr;
     Vector2 objCurrentPos;
     char tempFigureArr[OBJECT_SIZE][OBJECT_SIZE];
     Vector2 coordArray[OBJECT_SIZE][OBJECT_SIZE];
 }GameState;
 
 void InitGameState(GameState* state);
+void initMapArr(GameState* state);
+void initFilledObjectArr(GameState* state);
+void clearFailedState(char*** array, int index);
+void clearGameDoubleArray(char*** array ,int height);
 void clearGameState(GameState* state);
 
 #endif 
